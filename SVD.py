0f4d0e9773433883fd72a2e5fcb20b5b02b5bd81
dariosotelo <cesardariosotelo@gmail.com>
2023-11-15 21:16:57 -0600

    segundo commit

--- SVD.py
+++ SVD.py
@@ -6,3 +6,10 @@ Created on Tue Nov 14 15:53:22 2023
 @author: darios
 """
 
+from skimage.color import rgb2gray
+from skimage import data
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.linalg import svd
+
+U, singular, V_transpose = svd()
